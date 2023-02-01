740d90aa1fd95be42458baabcdb0725fc42a8d2e
vasyapeteckin <81539716+vasyapeteckin@users.noreply.github.com>
2023-02-01 17:32:13 +0300

    version 1.3.0
    Added new methods, description of methods, attribute typing, improved code readability

--- DiscordAPI.py
+++ DiscordAPI.py
@@ -1,12 +1,15 @@
 from time import sleep
+
 import requests as r
 from base64 import b64encode
+from urllib.parse import unquote
 
 
 class DiscordSession:
     """Create a discord session"""
 
-    def __init__(self, token):
+    def __init__(self, token: str):
+
         self.token = token
         self.s = r.Session()
         self.s.headers['authorization'] = token
@@ -14,105 +17,208 @@ class DiscordSession:
                                        'AppleWebKit/537.36' \
                                        ' (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
 
-    def info(self):
-        # Get user info by token
+    def info(self) -> dict:
+        """
+        Get user info
+        """
         return self.s.get('https://discord.com/api/v9/users/@me').json()
 
-    def change_username(self, new_username, password):
-        # Change Discord username
+    def change_username(self,
+                        new_username: str,
+                        password: str):
+        """
+        Change Discord username
+        :param new_username:
+        :param password:
+        :return:
+        """
         return self.s.patch(f'https://discord.com/api/v9/users/@me', json={"username": new_username,
                                                                            "password": password}).json()
 
-    def change_bio(self, new_bio):
-        # Change Discord user bio
+    def change_bio(self, new_bio: str) -> dict:
+        """
+        Change Discord user bio
+        :param new_bio:
+        :return:
+        """
         return self.s.patch(f'https://discord.com/api/v9/users/@me', json={"bio": new_bio}).json()
 
-    def change_avatar(self, image_url):
-        # Change Discord user avatar
+    def change_avatar(self, image_url: str) -> dict:
+        """
+        Change Discord user avatar
+        :param image_url:
+        :return:
+        """
         new_avatar = b64encode(r.get(image_url).content).decode("utf-8")
         _payload = {"avatar": f"data:image/png;base64,{new_avatar}"}
         return self.s.patch(f'https://discord.com/api/v9/users/@me', json=_payload).json()
 
-    def friends_list(self):
-        # Return friends list
+    def friends_list(self) -> dict:
+        """
+        :return: dict with friends list
+        """
         return self.s.get(f'https://discord.com/api/v9/users/@me/relationships').json()
 
-    def guilds_list(self):
-        # return guilds list
+    def guilds_list(self) -> dict:
+        """
+        :return: guilds dict
+        """
         return self.s.get('https://discord.com/api/v9/users/@me/guilds').json()
 
-    def guild_info(self, guild_id):
-        # return info about guild
+    def guild_info(self, guild_id: int) -> dict:
+        """
+        :param guild_id:
+        :return: info about guild
+        """
         return self.s.get(f'https://discord.com/api/v9/guilds/{guild_id}').json()
 
-    def guild_channels(self, guild_id):
-        # return info about guild
+    def get_roles(self, guild_id) -> dict:
+        """
+        :param guild_id:
+        :return:
+        """
+        return self.s.get(f'https://discord.com/api/v9/guilds/{guild_id}/roles').json()
+
+    def guild_channels(self, guild_id: int) -> dict:
+        """
+        :param guild_id:
+        :return: info about guild
+        """
         return self.s.get(f'https://discord.com/api/v9/guilds/{guild_id}/channels').json()
 
-    def leave_guild(self, guild_id):
-        # leave from guild
+    def leave_guild(self, guild_id: int) -> r.request:
+        """
+        leave from guild
+        :param guild_id:
+        :return: request obj
+        """
         return self.s.delete(f'https://discord.com/api/v9/users/@me/guilds/{guild_id}', json={"lurking": False})
 
-    def typing(self, channel_id, seconds=float(0.5)):
-        # Call typing function
-        if type(seconds) == float or type(seconds) == int:
-            response = self.s.post(f'https://discord.com/api/v9/channels/{channel_id}/typing')
-            sleep(seconds)
-            return response
-        else:
-            response = self.s.post(f'https://discord.com/api/v9/channels/{channel_id}/typing')
-            sleep(0.5)
-            return response
+    def typing(self,
+               channel_id: int,
+               seconds: float = 0.5) -> r.request:
+        """
+        calls the "typing" method in channel
+        :param channel_id:
+        :param seconds: delay time
+        :return:
+        """
+        response = self.s.post(f'https://discord.com/api/v9/channels/{channel_id}/typing')
+        sleep(seconds)
+        return response
 
-    def send_message(self, channel_id, text_message):
-        # Send message
+    def send_message(self,
+                     channel_id: int,
+                     text_message: str) -> dict:
+        """
+        Send message to the channel
+        :param channel_id:
+        :param text_message:
+        :return:
+        """
         _data = {'content': text_message, 'tts': False}
-        return self.s.post(f'https://discord.com//api/v9/channels/{channel_id}/messages', json=_data).json()
+        return self.s.post(f'https://discord.com/api/v9/channels/{channel_id}/messages', json=_data).json()
 
-    def reply_to_message_by_id(self, guild_id, channel_id, message_id, text_message):
-        _data = {
-                  "content": text_message,
-                  "tts": False,
-                  "message_reference": {
-                    "guild_id": guild_id,
-                    "channel_id": channel_id,
-                    "message_id": message_id
-                  }
-                }
-        return self.s.post(f'https://discord.com//api/v9/channels/{channel_id}/messages', json=_data).json()
+    def reply_to_message(self,
+                         guild_id: int,
+                         channel_id: int,
+                         message_id: int,
+                         text_message: str):
+        _payload = {
+            "content": text_message,
+            "tts": False,
+            "message_reference": {
+                "guild_id": guild_id,
+                "channel_id": channel_id,
+                "message_id": message_id
+            }
+        }
+        return self.s.post(f'https://discord.com//api/v9/channels/{channel_id}/messages', json=_payload).json()
 
-    def get_history_by_channel_id(self, channel_id, messages_limit: int = 50):
-        # Get History by channel id
+    def get_history_by_channel_id(self,
+                                  channel_id: int,
+                                  messages_limit: int = 50) -> dict:
+        """
+        Get History by channel id
+        :param channel_id:
+        :param messages_limit: Number of messages in one request
+        :return:
+        """
         return self.s.get(
             f'https://discord.com/api/v9/channels/{channel_id}/messages?limit={int(messages_limit)}').json()
 
-    def get_history_by_user_id(self, guilds_id, user_id, offset=0):
-        # get last 25 messages from user by ID
+    def get_history_by_user_id(self,
+                               guild_id: int,
+                               user_id: int,
+                               offset: int = 0) -> dict:
+        """
+        get messages from user by ID
+        :param guild_id:
+        :param user_id:
+        :param offset: offset number of messages from the last message
+        :return:
+        """
         response = self.s.get(
-            f"https://discord.com/api/v9/guilds/{guilds_id}/messages/search?author_id={user_id}&offset={int(offset)}"
+            f"https://discord.com/api/v9/guilds/{guild_id}/messages/search?author_id={user_id}&offset={offset}"
         )
-        # TODO: Get all history by user_id
         return response.json()
 
-        # TODO: Combine methods PUT/DELETE
-    def react_on_message(self, channel_id, message_id, react='🎉'):
-        # Reaction on message by message_id
+    def react_on_message(self,
+                         channel_id: int,
+                         message_id: int,
+                         react: str = '🎉') -> dict:
+        """
+        Reaction on message
+        :param channel_id:
+        :param message_id:
+        :param react:
+        :return:
+        """
         response = self.s.put(
             f'https://discord.com/api/v9/channels/{channel_id}/messages/{str(message_id)}/reactions/{react}/@me')
         try:
-            return {"status": response, "username": self.info()['username'], "react": react}
+            return {"status": response,
+                    "username": self.info()['username'],
+                    "react": unquote(react)}
         except KeyError:
-            return {"status": self.info(), "token": self.token}
+            return {"status": self.info(),
+                    "token": self.token}
 
-    def delete_react_from_message(self, channel_id, message_id, react='🎉'):
-        # Delete reaction from message by message_id
+    def delete_react_from_message(self,
+                                  channel_id: int,
+                                  message_id: int,
+                                  react: str = '🎉') -> dict:
+        """
+        Delete reaction from message by message_id
+        :param channel_id:
+        :param message_id:
+        :param react:
+        :return:
+        """
         response = self.s.delete(
             f'https://discord.com/api/v9/channels/{channel_id}/messages/{str(message_id)}/reactions/{react}/@me')
         try:
-            return {"status": response, "username": self.info()['username'], "react": react}
+            return {"status": response.status_code,
+                    "username": self.info()['username'],
+                    "react": unquote(react)}
         except KeyError:
-            return {"status": self.info(), "token": self.token}
+            return {"status": response.status_code,
+                    "token": self.token}
 
-    def delete_message_by_id(self, channel_id, message_id):
-        # Delete message by message id
-        return self.s.delete(f'https://discord.com/api/v9/channels/{channel_id}/messages/{message_id}')
+    def delete_message_by_id(self,
+                             channel_id: int,
+                             message_id: int) -> int:
+        """
+        Delete message
+        :param channel_id:
+        :param message_id:
+        :return:
+        """
+        return self.s.delete(f'https://discord.com/api/v9/channels/{channel_id}/messages/{message_id}').status_code
+
+    def delete_channel_by_id(self, channel_id) -> int:
+        """
+        :param channel_id:
+        :return:
+        """
+        return self.s.delete(f'https://discord.com/api/v9/channels/{channel_id}').status_code
